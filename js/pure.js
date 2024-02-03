0b1da19907b1ef6af76fbc36005a1860e74d980d
dCOMMICS <sanwurld@gmail.com>
2024-02-03 20:32:20 +0300

    lecture 101 answers

--- js/pure.js
+++ js/pure.js
@@ -5,3 +5,11 @@
 
 let firstName = " Jerry";
 console.log(firstName); 
+
+
+// lecture 101
+
+let countryName = "United States";
+let continent = " North America";
+let population =  331900000
+console.log(countryName, continent, population)
