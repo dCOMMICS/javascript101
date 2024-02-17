16eaad01fd8ffa01e6e1d0c1a8704ab392215234
dCOMMICS <sanwurld@gmail.com>
2024-02-18 00:15:14 +0300

    initial commit

--- js/pure.js
+++ js/pure.js
@@ -83,7 +83,8 @@ console.log(x)
 console.log(age_miley > age_pete);
 console.log(age_pete >= 18);
 
-const isFullage = age_pete >= 18 ; 
+const isFullage = age_pete >= 18 ;
+ 
 
 
 // lecture : let const and var
